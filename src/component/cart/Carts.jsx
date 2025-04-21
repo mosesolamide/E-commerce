4072bad23dc10e5e6ac58016458c3d6a3967e958
mosesolamide <olamidemoses132@gmail.com>
2025-04-21 23:23:23 +0100

    new

--- src/component/cart/Carts.jsx
+++ src/component/cart/Carts.jsx
@@ -1,4 +1,4 @@
-import React,{ useContext } from 'react'
+import React,{ useContext, useState, useEffect } from 'react'
 import { UserContext } from '../../App'
 import { GiShoppingCart } from "react-icons/gi"
 import Product from './Products'
@@ -6,9 +6,20 @@ import { Link } from 'react-router-dom'
 
 export default function Carts(){
     const { user, cart, loading } = useContext(UserContext)
-    console.log(cart.length)
+    const [ totalPrice,setTotalPrice ] = useState(null)
+
+    useEffect(() => {
+        const total = cart.reduce( (acc,item) => {
+            const price = item.product?.discountPrice || item.product?.normalPrice || 0
+            const qty = item.quantity || 1
+            return acc + price * qty 
+        },0)
+        setTotalPrice(total)
+      }, [cart])
+    
     return(
-            loading ? (
+        <>
+            {loading ? (
                     <div className='flex justify-center items-center h-[50vh] gap-2'>
                         <span className='w-[35px] h-[35px] border-[5px] border-black border-solid border-b-transparent rounded-[50%] inline-block box-border animate-spin'></span> <br />
                         <p className='text-sm sm:text-md md:text-2xl'>Hold While Your Cart is Loading...</p>
@@ -22,30 +33,64 @@ export default function Carts(){
                     </div>
                 </div>
             ) :(
-                <table className='max-w-[900px] mx-auto mt-6'>
-                    <thead className=''>
-                        <tr 
-                            className='grid grid-cols-4
-                            justify-between place-items-center text-[7px] md:text-[9px] font-medium py-3 shadow-sm rounded-[2px]'
-                        >
-                            <td>Product</td>
-                            <td>Price</td>
-                            <td>Quantity</td>
-                            <td>Subtotal</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            cart.map( (item,index) =>(
-                                <Product 
-                                    key={index}
-                                    item={item}
-                                    id={item.id}
-                                />
-                            ))
-                        }
-                    </tbody>
-                </table>
-            )
+                <div className='mx-auto max-w-[1000px] '>
+                    <table className='mt-6 w-full'>
+                        <thead className=''>
+                            <tr 
+                                className='grid grid-cols-4
+                                justify-between place-items-center text-[7px] md:text-[9px] font-medium py-3 shadow-sm rounded-[2px]'
+                            >
+                                <td>Product</td>
+                                <td>Price</td>
+                                <td>Quantity</td>
+                                <td>Subtotal</td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                cart.map( (item,index) =>(
+                                    <Product 
+                                        key={index}
+                                        item={item}
+                                        id={item.id}
+                                    />
+                                ))
+                            }
+                        </tbody>
+                        <button className='border border-black/50 rounded-[4px] text-xs px-4 py-2 mt-4'>Return To Shop</button>
+                    </table>
+                    <div className='mt-6 flex flex-col justify-center items-center md:flex-row md:justify-around gap-2 w-full'>
+                        <div className='flex gap-3'>
+                            <input type="text" placeholder='Coupon Code' className='border border-black/50 rounded-[2px] indent-2 w-[180px] h-[40px]' />
+                            <button className='bg-[#DB4444] px-4 py-2 text-white rounded-[4px] text-xs h-[40px]'>Apply Coupon</button>
+                        </div>
+                        <div className='mt-5 md:mt-0 w-[400px]'>
+                            <div className='border p-3 rounded-[2px] text-center'>
+                                <h1 className='mb-3'>Cart Total</h1>
+                                <div className='flex justify-between text-xs font-[400]'>
+                                    <span>Subtotal:</span>
+                                    <span>${totalPrice}</span>
+                                </div>
+                                <hr className='my-3 border-[1] border-black/50' />
+                                <div className='flex justify-between text-xs font-[400]'>
+                                    <span>Shipping:</span>
+                                    <span>Free</span>
+                                </div>
+                                <hr className='my-3 border-[1] border-black/50' />
+                                <div className='flex justify-between text-xs font-[400]'>
+                                    <span>Total:</span>
+                                    <span>${totalPrice}</span>
+                                </div>
+                                <button 
+                                    className='bg-[#DB4444] px-4 py-2 text-white rounded-[4px] text-xs h-[40px] mt-5'
+                                >
+                                    Process to checkout
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            )}
+        </>
     )
 }
--- src/component/cart/Products.jsx
+++ src/component/cart/Products.jsx
@@ -15,7 +15,8 @@ const Product = memo(({item,id}) => {
 
     return(
         <tr
-            className=' grid grid-cols-4 justify-between place-items-center text-[8px] md:text-[9px] font-medium py-3 px-3 shadow-sm rounded-[2px] mt-6'
+            className=' grid grid-cols-4 justify-between place-items-center text-[8px] md:text-[9px] 
+            font-medium py-3 px-3 shadow-sm rounded-[2px] mt-6'
         >
             <td className='flex items-center gap-1'>
                 <img src={`images/${item.imagePath}/${item.product.img}`} alt="image of product" className='w-[15px] sm:w-[17px] md:w-[20px]' />
