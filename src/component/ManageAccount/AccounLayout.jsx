adde82276460a58cb876184a074f5339f834dae8
mosesolamide <olamidemoses132@gmail.com>
2025-04-29 23:51:27 +0100

    new

--- src/component/ManageAccount/AccounLayout.jsx
+++ src/component/ManageAccount/AccounLayout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom"
 
 export default function AccountLayout(){
     return(
-        <div className="py-10 px-6 flex flex-col w-full md:flex-row items-start gap-10">
+        <div className="py-10 px-6 flex flex-col w-full md:flex-row md:items-start gap-10">
             <SideBar />
             <Outlet />
         </div>
